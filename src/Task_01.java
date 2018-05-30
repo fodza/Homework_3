26af3a5ef906b3f5fd614d63790c5d1a416d440b
Agnese <fodza@inbox.lv>
2018-05-30 18:31:18 +0300

    Task_1
    1.uzdevums

---
+++ src/Task_01.java
@@ -0,0 +1,25 @@
+package day5;
+
+import java.util.Scanner;
+
+public class Testi {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		
+		
+		System.out.println("Ievadi skaitli i: ");
+		int i = sc.nextInt();{
+		
+				
+			for (int ij = 0; ij <= 5; ij++) { 
+				System.out.println(i + "x" + ij+ " = " + ij*i);
+			
+			}
+		}
+		
+		}
+	
+	}
+
